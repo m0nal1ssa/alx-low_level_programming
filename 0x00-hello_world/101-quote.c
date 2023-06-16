65f80a1cece3620aba0f4e8b106dacdec239e3f5
m0nal1ssa <monalis.femi@gmail.com>
2023-06-15 21:19:14 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
-
 /**
  * main - starting point
  * Description: writing out a quote with write function
  * Return: return (1) to exit code to show an error
  */
-
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 59);
+	write(1, qu, 59);
 	return (1);
 }
