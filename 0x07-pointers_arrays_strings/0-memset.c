64d7f5afad939fb1b8185bf3d5b05ccceb5dd54d
m0nal1ssa <monalis.femi@gmail.com>
2023-07-04 15:40:53 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 include "main.h"
+
 /**
  * _memset - Entry
  * @s: pointed
@@ -11,6 +12,9 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
+	}
+
 	return (s);
 }
