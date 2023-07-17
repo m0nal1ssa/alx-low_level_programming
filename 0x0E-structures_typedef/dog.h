ab04432d7d19fac638dfc6b9c909b571ff11d4fc
m0nal1ssa <monalis.femi@gmail.com>
2023-07-17 16:35:25 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - blueprint for a puppy
+ * @name: pointer to name
+ * @age: age of puppy
+ * @owner: pointer to owner of puppy
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
