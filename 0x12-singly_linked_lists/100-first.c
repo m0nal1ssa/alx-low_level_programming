34b3ecd2763fb96c08b68b836935cfc13330586d
m0nal1ssa <monalisa.efua@gmail.com>
2023-07-27 02:43:14 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+/***ADVANCED TASK 100***
+ * File: 100-first.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - Prints a string before main function is executed.
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
