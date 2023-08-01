d425147f83e33522e40e9b76455da7ffa853b5ce
m0nal1ssa <monalisa.efua@gmail.com>
2023-08-01 15:38:47 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,62 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * print_listint_safe - print list
- * @head: head node
- * Return: amount of nodes in list
+ * find_loop - Helper function to find a loop in a linked list
+ * @head: Pointer to the head of the linked list
+ * Return: Pointer to the node causing the loop, or NULL if no loop
+ */
+listint_t *find_loop(const listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	slow = (listint_t *)head;
+	fast = (listint_t *)head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = (listint_t *)head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t linked list with loop handling
+ * @head: Pointer to the head of the linked list
+ * Return: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int size = 0, i;
-	const listint_t *tmp[100];
+	size_t count = 0;
+	listint_t *loop_node = find_loop(head);
 
-	if (!head)
-		exit(98);
-
-	while (head)
+	while (head != NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (tmp[i] == head)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				return (size);
-			}
-		}
 		printf("[%p] %d\n", (void *)head, head->n);
-		tmp[size] = head;
-		size++;
+		count++;
+
+		if (head == loop_node)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
+
 		head = head->next;
 	}
-	return (size);
+
+	return (count);
 }
