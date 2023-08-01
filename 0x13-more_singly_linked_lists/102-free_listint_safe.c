db7b349e8e44b5a9264813a2aa9eae8b3ff2808a
m0nal1ssa <monalisa.efua@gmail.com>
2023-08-01 15:36:26 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,44 +1,44 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_listint_safe - This frees a listint_t linked list.
- * @h: Pointer to a pointer to the head of the list.
+ * find_listint_loop - Finds the loop in a linked list.
+ * @head: Pointer to the head of the list.
  *
- * Return: The size of the list that was freed.
+ * Return: The address of the node where the loop starts,
+ *         or NULL if there is no loop.
  */
-size_t free_listint_safe(listint_t **h)
+listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *current, *temp;
-	size_t count = 0;
+	listint_t *tortoise, *hare;
 
-	if (h == NULL)
-		return (0);
+	if (head == NULL || head->next == NULL)
+		return (NULL);
 
-	current = *h;
-	while (current != NULL)
+	tortoise = head->next;
+	hare = head->next->next;
+
+	while (hare && hare->next)
 	{
-		count++;
-		/* Save next pointer before you free current node */
-		temp = current->next;
-
-		/* Set the current node's next to NULL */
-		current->next = NULL;
-
-		/* Free up the current node */
-		free(current);
-
-		/* Move to the next node */
-		current = temp;
-
-		/* If theres a repeation, break the loop */
-		if (current == *h)
+		if (tortoise == hare)
 		{
-			/* Set head to NULL to show the list is freed */
-			*h = NULL;
-			break;
+			/* Reset one of the pointers and start from the head */
+			tortoise = head;
+
+			/* Move both pointers at the same pace until they meet again */
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			/* Return the address of the node where the loop starts */
+			return (tortoise);
 		}
+
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 	}
 
-	return (count);
+	/* If no loop is found, return NULL */
+	return (NULL);
 }
